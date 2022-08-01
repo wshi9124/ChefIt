36f2bdde581fd2cdedcde45514e810bf17dfcbfe
Willie Shi <wshi9124@gmail.com>
2022-07-31 20:45:14 -0400

    working on form for login and css for header

--- src/commonComponents/Logo.jsx
+++ src/commonComponents/Logo.jsx
@@ -4,13 +4,9 @@ import { useNavigate } from 'react-router-dom';
 function Logo() {
   const navigate = useNavigate();
   return (
-    <div className="logo">
-      <h1> Chef It This is the Header add pictures</h1>
-      <h2>Your go to place to rent personal chefs</h2>
-      <h2>
-        Slogan: Why cook when you can chef it (we can think of something better
-        later xd)
-      </h2>
+    <div className="logo-container">
+      <img src="images/chefitlogo.png" alt="chef it logo" />
+      <h1> Chef It </h1>
       <button type="button" onClick={() => { navigate('/login'); }}>Log Out</button>
       <button type="button" onClick={() => { navigate('/updateinfo'); }}>Update Personal Info</button>
     </div>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,6 +7,9 @@ function Login() {
   return (
     <div className="login">
       <div className="loginform">
+        <h1>Chef It</h1>
+        <h1>Your go to place to hire personal chefs</h1>
+        <h1>Why cook when you can chef it!</h1>
         <button type="button" onClick={() => { navigate('/user'); }}>Login as User </button>
         <button type="button" onClick={() => { navigate('/chef'); }}>Login as Chef</button>
         <CreateAccountModal />
