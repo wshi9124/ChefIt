818d5b136f7cbe301c9f9daa702b0a3d9ea8ea48
Willie Shi <wshi9124@gmail.com>
2022-08-01 11:45:11 -0400

    nothing

--- src/commonComponents/Logo.jsx
+++ src/commonComponents/Logo.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function Logo() {
-  const navigate = useNavigate();
   return (
     <div className="logo-container">
       <img src="images/chefitlogo.png" alt="chef it logo" />
--- src/commonComponents/Request/index.jsx
+++ src/commonComponents/Request/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Request() {
   return (
-    <h1> This is the request JSX that will be shared with both chefs and users so code can be more dry</h1>
+    <h1> This is the Profile JSX that will be shared with both chefs and users so code can be more dry</h1>
   );
 }
 
