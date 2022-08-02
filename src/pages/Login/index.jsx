cbfd3a060b9be41a05559d9fcad4b0b32702d6db
Willie Shi <wshi9124@gmail.com>
2022-08-02 17:03:00 -0400

    worked on login authentification

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,34 +1,90 @@
-import React from 'react';
+import React, {
+  useRef, useState, useEffect, useContext,
+} from 'react';
 import { useNavigate } from 'react-router-dom';
+import AuthContext from './AuthProvider';
 import CreateAccountModal from './CreateAccountModal';
 
 function Login() {
   const navigate = useNavigate();
+
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [accountType, setAccountType] = useState('customer');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrorMessage('');
+  }, [user, password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, password);
+    setUser('');
+    setPassword('');
+    setSuccess(true);
+  };
+
   return (
     <section className="login">
       <div className="login-form">
         <div>
           <div className="login-img">
             <img src="images/chefitlogo.png" alt="chef it logo" className="login-logo" />
+            <p ref={errRef} className={errorMessage ? 'errorMessage' : 'offscreen'} aria-live="assertive">{errorMessage}</p>
             <h1 className="loginHeader">Chef It</h1>
             <h2 className="loginHeader">Why cook, when you can chef it!</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-floating mb-3">
-                <input type="username" className="form-control rounded-0" placeholder="Username" id="floatingInput" />
-                <label htmlFor="floatingInput">Username</label>
+                <input
+                  type="text"
+                  className="form-control rounded-0"
+                  placeholder="Username"
+                  id="username"
+                  ref={userRef}
+                  autoComplete="off"
+                  onChange={(e) => setUser(e.target.value)}
+                  value={user}
+                  required
+                />
+                <label htmlFor="username">Username</label>
               </div>
               <div className="form-floating mb-3">
-                <input type="username" className="form-control rounded-0" placeholder="Password" id="floatingPassword" />
-                <label htmlFor="floatingInput">Password</label>
+                <input
+                  type="password"
+                  className="form-control rounded-0"
+                  placeholder="Password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                />
+                <label htmlFor="password">Password</label>
               </div>
-              <select className="form-select" aria-label="Default select example" type="select" name="logintype">
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                type="select"
+                name="logintype"
+                value={accountType}
+                onChange={(e) => setAccountType(e.target.value)}
+              >
                 <option value="customer">Customer</option>
                 <option value="user">Chef</option>
                 <label htmlFor="form-select">Type of User:</label>
               </select>
-              <p></p>
+              <p />
               <div className="d-grid gap-2 mb-3">
-                <button type="button" className="btn btn-dark btn-lg border-0 rounded-0" value="Login">Login</button>
+                <button type="submit" className="btn btn-dark btn-lg border-0 rounded-0" value="Login">Login</button>
               </div>
               <p>Dont have an account?</p>
               <CreateAccountModal />
--- src/pages/User/UserHome.jsx
+++ src/pages/User/UserHome.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LoadMap } from './Map';
 import SideBar from './sidebar';
 
