b2ddb7afc8720df398098b544b0c3f6b6cff667a
Sean Booker Jr <seanbookerjunior@gmail.com>
2022-08-03 14:28:09 -0400

    chef profile

--- src/pages/Chef/index.jsx
+++ src/pages/Chef/index.jsx
@@ -1,14 +1,16 @@
+import { Button } from 'bootstrap';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Logo from '../../commonComponents/Logo';
 import ChefNavBar from './ChefNavbar';
+import EditChefProfileModal from './EditChefProfileModal';
 
 function Chef() {
 
 const [info, setInfo] = useState([])
 
 useEffect(()=> {
-  fetch('http://localhost:9292/chef/1')
+  fetch('http://localhost:9292/chef/info')
   .then(res => res.json())
   .then(infoData => {
     setInfo(infoData)
@@ -20,23 +22,44 @@ useEffect(()=> {
       <Logo />
       <ChefNavBar />
       <div className='chef-home'>
-        <h1>Your Profile</h1>
-        
+        <h1>Your Profile</h1> 
+        <EditChefProfileModal />
           <div className='image-overlay-container'>
-            <img className='rounded-image' src = "https://i.pinimg.com/originals/6b/e1/84/6be1847b56e3832a614699142d1e09d8.jpg" width="600" height="400"/>
+            <img className='rounded-image' src = "https://img.freepik.com/free-photo/portrait-happy-male-chef-dressed-uniform_171337-5354.jpg?w=2000" width="400" height="300"/>
             <div className='text-overlay-container'>
-                <h2>Username</h2>
-                <div>{info.username}</div>
+                <div className='infoText'>
+                <h3>Username</h3>
+                <p>{info.username}</p>
+                </div>
+                <div className='infoText'>
+                  <h3>Full Name</h3>
+                  <p>{info.first_name+" "+info.last_name}</p>
+                </div>
+                <div className='infoText'>
+                  <h3>Password</h3>
+                  <p>{info.password}</p>
+                </div>
+                <div className='infoText'>
+                  <h3>Phone Number</h3>
+                  <p>{info.phone}</p>
+                </div>
+                <div className='infoText'>
+                  <h3>Email</h3>
+                  <p>{info.email}</p>
+                </div>
+                <div className='infoText'>
+                  <h3>Location</h3>
+                  <p>{"Longitude:"+" "+info.longitude+" "+"Latitude:"+" "+info.latitude}</p>
+                </div>
+                <div className='infoText'>
+                <h3>Your Price</h3>
+                  <p>{info.chef_price}</p>
+                </div>
             </div>
           </div>
-            <div>
-              <p>{info.first_name+" "+info.last_name}</p>
-
-            </div>
-            <div>
-
-            </div>
+            
         </div>
+
     </>
   );
 }
