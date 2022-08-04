acf59488c4204287290ac116eade2dff72acb441
alextaylor <alextaylor515@gmail.com>
2022-08-04 17:28:02 -0400

    Profile

---
+++ src/pages/Chef/PostCard.jsx
@@ -0,0 +1,17 @@
+
+
+function PostCard({img,name,deleteCallback,id}) {
+
+    function handledelete() {
+        deleteCallback(id)
+    }
+    return (
+            <div>
+                <img src={img}/>
+                <p>{name}</p>
+                <button onClick={handledelete}>Delete</button>
+            </div>
+        );
+}
+
+export default PostCard
--- src/pages/Chef/index.jsx
+++ src/pages/Chef/index.jsx
@@ -4,61 +4,38 @@ import ChefNavBar from './ChefNavbar';
 import EditChefProfileModal from './EditChefProfileModal';
 import AddPictureModal from './AddPictureModal';
 import AuthContext from '../Login/AuthProvider';
-
+import ProfilePosts from './ProfilePosts';
+const img = "https://media.istockphoto.com/photos/old-grunge-dark-textured-wooden-backgroundthe-surface-of-the-old-picture-id865432924?k=20&m=865432924&s=612x612&w=0&h=fCWAbNMq85WP8oWie-DtmZmDzJxV5c61rU9TmG2uPdk="
+const placeholder = "https://idea7.co.uk/wp-content/uploads/2021/02/placeholder-250x250-1.png"
 function Chef() {
-  const [info, setInfo] = useState([]);
-  const { auth } = useContext(AuthContext);
+  // const [info, setInfo] = useState([]);
+  // const { auth } = useContext(AuthContext);
 
   return (
     <>
-      <Logo />
-      <ChefNavBar />
-      <div className="float-container">
-        <div className="float-child">
-          <h1>Your Profile</h1>
-          <EditChefProfileModal auth={auth} />
-          <div className="image-overlay-container">
-            <img className="rounded-image" src={auth.prof_pic} width="400" height="300" placeholder="Chef Profile Pic" style={{ objectFit: 'cover' }} />
+    <ChefNavBar/>
+    <Logo/>
+      <div className="image-overlay-container">
+            <img src={img} alt='background'/>
             <div className="text-overlay-container">
-              <div className="infoText">
-                <h3>Username</h3>
-                <p>{auth.username}</p>
+              <div className='header-info-card'>
+                  <div className='info-popup-item large-gap'>
+                    <img className='rounded-image' src={placeholder} width="150px" height="150px"/>
+                    <h4>Username</h4>
               </div>
-              <div className="infoText">
-                <h3>Full Name</h3>
-                <p>{`${auth.first_name} ${auth.last_name}`}</p>
-              </div>
-              <div className="infoText">
-                <h3>Price</h3>
-                <p>{auth.chef_price}</p>
-              </div>
-              <div className="infoText">
-                <h3>Phone Number</h3>
-                <p>{auth.phone}</p>
-              </div>
-              <div className="infoText">
-                <h3>Email</h3>
-                <p>{auth.email}</p>
-              </div>
-              <div className="infoText">
-                <h3>Location</h3>
-                <p>{'Longitude:' + ` ${auth.longitude} ` + 'Latitude:' + ` ${auth.latitude}`}</p>
+              <div className="text-inner-container">
+                <div className='info-popup-item large-gap'>
+                  <p> Full name</p>
+                  <p> Email </p>
+                  <p> Phone number</p>
+                </div>
+                <p> Location </p>
+                <p> Price </p>
               </div>
             </div>
           </div>
-        </div>
-        <div className="float-child">
-          <h1> Your Portfolio</h1>
-          <div className="float-container">
-            <AddPictureModal />
-            <div className="float-child" />
-            <div className="float-child" />
-
-          </div>
-        </div>
-
       </div>
-
+      <ProfilePosts />
     </>
   );
 }
