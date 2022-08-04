8dc428bb44130f1e2c17ba6c85c14780b9325d5c
Willie Shi <wshi9124@gmail.com>
2022-08-04 15:21:52 -0400

    added buttons

--- src/pages/Chef/moreinfopopup.jsx
+++ src/pages/Chef/moreinfopopup.jsx
@@ -1,40 +1,42 @@
-import { Modal,Button } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import StarFill from '../../commonComponents/starfill';
 
 function MoreInfoPopup({
-  show, onHide, fullname = '', user_comments=[],
+  show, onHide, fullname = '', user_comments = [],
 }) {
-
-  const filteredrating = " "|| user_comments.map((comment) => comment.rating).reduce((sum, ele) => sum + ele);
+  const filteredrating = ' ' || user_comments.map((comment) => comment.rating).reduce((sum, ele) => sum + ele);
 
   const reviewList = user_comments.map((review) => (
     <div className="info-popup-item" key={review.id}>
       <p>{review.comment}</p>
-      <StarFill iter={review.rating}/>
+      <StarFill iter={review.rating} />
     </div>
-    ))
-    return (
-        <Modal
-        show={show}
-        onHide={onHide}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <h2>{fullname} </h2>
-            <StarFill iter={filteredrating}/>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            {reviewList}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+  ));
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <h2>
+            {fullname}
+            {' '}
+          </h2>
+          <StarFill iter={filteredrating} />
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {reviewList}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export default MoreInfoPopup;
